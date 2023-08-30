f15cc7b98111aa91133d0959ae636df228ff5e4a
Surya prakashlokula <59243908+suryadevelope@users.noreply.github.com>
2023-08-30 12:31:26 +0530

    Update build.sh

--- external/libvmc/packages/zlib/build.sh
+++ external/libvmc/packages/zlib/build.sh
@@ -1,5 +1,5 @@
-PACKAGE_VERSION="1.2.13"
-PACKAGE_SRCURL="https://www.zlib.net/zlib-$PACKAGE_VERSION.tar.xz"
+PACKAGE_VERSION="1.2.11"
+PACKAGE_SRCURL="https://zlib.net/fossils/zlib-$PACKAGE_VERSION.tar.gz"
 PACKAGE_SHA256="d14c38e313afc35a9a8760dadf26042f51ea0f5d154b0630a31da0540107fb98"
 
 builder_step_configure() {
